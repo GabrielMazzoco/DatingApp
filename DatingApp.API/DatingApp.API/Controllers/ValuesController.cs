226b04731395b5d6ad9b0975df44f91184028bff
GabrielMazzoco <gabrielmazzoco@live.com>
2018-12-25 13:50:16 -0200

    Authentication on the aplication

--- DatingApp.API/DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/DatingApp.API/Controllers/ValuesController.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     [ApiController]
     public class ValuesController : ControllerBase
     {
@@ -27,6 +27,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
