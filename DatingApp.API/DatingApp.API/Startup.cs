679d7de854aeecdad91c6bd93b7203c776717ab3
GabrielMazzoco <gabrielmazzoco@live.com>
2018-12-27 22:29:54 -0200

    End of Section 4

--- DatingApp.API/DatingApp.API/Startup.cs
+++ DatingApp.API/DatingApp.API/Startup.cs
@@ -52,10 +52,10 @@ namespace DatingApp.API
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//                app.UseHsts();
+                //app.UseHsts();
             }
 
-//            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
